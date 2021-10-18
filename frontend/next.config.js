2ecc7d89aa5425b22494b27f02186c9761231db2
B. Düdük <bunyamin.duduk@hva.nl>
2021-10-18 19:54:46 +0200

    Added and fixed docker, front end and back end

---
+++ frontend/next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  reactStrictMode: true,
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000,
+      aggregateTimeout: 300,
+    };
+    return config;
+  },
+
+  env: {
+    BACK_END: process.env.BACK_END,
+  },
+};
