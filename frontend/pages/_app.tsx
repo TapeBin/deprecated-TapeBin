3eb39b31ab0b7c1f8b0b0fcbb40fece894ff1c40
Bunyamin Duduk <bunyamin.duduk@hva.nl>
2021-10-22 22:36:44 +0200

    Added TailwindCSS

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/dist/shared/lib/router/router";
+import "../styles/global.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
