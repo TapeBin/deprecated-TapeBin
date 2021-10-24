f84da19a6f413c136f6f86af023ad48dcef2a6d5
B. Düdük <bunyamin.duduk@hva.nl>
2021-10-24 18:07:48 +0200

    Added bin item and moved to seperate folder

---
+++ frontend/components/form/binlist/BinList.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const BinList = () => {
+  return (
+    <div className="min-w-full bg-gray-600 text-white overflow-x-auto"></div>
+  );
+};
+
+export default BinList;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,6 +3,7 @@ import Editor from "../components/editor/Editor";
 import Navbar from "../components/navbar/Navbar";
 import dynamic from "next/dynamic";
 import Form from "../components/form/Form";
+import BinList from "../components/form/binlist/BinList";
 const DynamicEditor = dynamic(
   () => {
     return import("../components/editor/Editor");
@@ -15,6 +16,7 @@ const Index = (props: any) => {
     <div className="flex flex-col" style={{ width: "100vw", height: "100vh" }}>
       <Navbar />
       <Form />
+      <BinList />
       <DynamicEditor />
     </div>
   );
