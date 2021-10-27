d2337b2c5b010452b4336b31c29fe5af1919db56
Bunyamin Duduk <bunyamin.duduk@hva.nl>
2021-10-27 20:20:10 +0200

    Finished footer

--- frontend/components/footer/Footer.tsx
+++ frontend/components/footer/Footer.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import SocialMediaIcon from "./SocialMediaIcon";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center justify-end">
-      <div>icons</div>
-      <div>Copyright</div>
+    <footer className="flex flex-col items-center justify-end space-y-2 text-center opacity-60 hover:opacity-100 transition duration-300 text-gray-200">
+      <div className="flex flex-row items-center space-x-6">
+        <SocialMediaIcon
+          src="./images/social_media/discord.svg"
+          alt="Discord"
+        />
+        <SocialMediaIcon src="./images/social_media/github.svg" alt="Github" />
+        <SocialMediaIcon
+          src="./images/social_media/twitter.svg"
+          alt="Twitter"
+        />
+      </div>
+      <div>Copyright © 2021, TapeBin. All Rights Reserved.</div>
     </footer>
   );
 };
--- frontend/components/footer/SocialMediaIcon.tsx
+++ frontend/components/footer/SocialMediaIcon.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 
-const SocialMediaIcon = () => {
-  return <div></div>;
+type SocialMediaIconProsp = {
+  src: string;
+  alt: string;
+  width?: string;
+  height?: string;
+  url?: string;
+};
+
+const SocialMediaIcon: FunctionComponent<SocialMediaIconProsp> = (
+  props: SocialMediaIconProsp
+) => {
+  return (
+    <img
+      src={props.src}
+      alt={props.alt}
+      style={{ width: props.width, height: props.height, cursor: "pointer" }}
+      onClick={() => window.open(props.url, "_blank")}
+    />
+  );
+};
+
+SocialMediaIcon.defaultProps = {
+  width: "25px",
+  height: "25px",
+  url: "https://tapeb.in",
 };
 
 export default SocialMediaIcon;
