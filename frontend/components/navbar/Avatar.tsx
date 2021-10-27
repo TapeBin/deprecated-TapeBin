855820556053674f0e86f432a634c5dc306fd83a
Bunyamin Duduk <bunyamin.duduk@hva.nl>
2021-10-27 17:03:10 +0200

    Cleared out

--- frontend/components/navbar/Avatar.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Avatar = () => {
-  return <img src="./images/avatar.svg" alt="Profile" className="h-9 inline" />;
-};
-
-export default Avatar;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import Editor from "../components/editor/Editor";
 import Navbar from "../components/navbar/Navbar";
 import dynamic from "next/dynamic";
-import Form from "../components/form/Form";
-import BinList from "../components/form/binlist/BinList";
 const DynamicEditor = dynamic(
   () => {
     return import("../components/editor/Editor");
@@ -15,9 +13,9 @@ const Index = (props: any) => {
   return (
     <div className="flex flex-col" style={{ width: "100vw", height: "100vh" }}>
       <Navbar />
-      <Form />
-      <BinList />
-      <DynamicEditor />
+      {/* <Form /> */}
+      {/* <BinList /> */}
+      {/* <DynamicEditor /> */}
     </div>
   );
 };
