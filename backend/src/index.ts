595519f19fb0663678e1b80bf6bc5e1d1dc80c19
B. Düdük <bunyamin.duduk@hva.nl>
2021-10-29 16:51:14 +0200

    Connecting to database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,22 @@
 import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 const app = express();
 const PORT = 5001 || process.env.PORT;
+
+app.use(
+  cors({
+    origin: `${process.env.FRONT_END}`,
+    credentials: true,
+  })
+);
+
+mongoose
+  .connect(
+    `${process.env.START_MONGODB}${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}${process.env.END_MONGODB}`
+  )
+  .then(() => console.log("Connected to database!"))
+  .catch((err) => console.log(`Database error: ${err}`));
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,7 +2,20 @@ import { AppProps } from "next/dist/shared/lib/router/router";
 import "../styles/global.css";
 import "@fontsource/roboto";
 import "@fontsource/lobster";
+import { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [isLoaded, setLoaded] = useState(false);
+  useEffect(() => {
+    axios({
+      method: "GET",
+      withCredentials: true,
+      url: `${process.env.BACK_END}/user`,
+    }).then((response: AxiosResponse) => {
+      setLoaded(true);
+    });
+  }, []);
+
+  return isLoaded && <Component {...pageProps} />;
 }
