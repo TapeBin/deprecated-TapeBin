4945fe493dbfaf17225f921f9ece4b659f5c91ea
B. Düdük <bunyamin.duduk@hva.nl>
2021-10-30 16:32:24 +0200

    Added top profile

--- frontend/components/profile/Profile.tsx
+++ frontend/components/profile/Profile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Profile = () => {
-  return <div></div>;
+  return <div className="flex flex-col"></div>;
 };
 
 export default Profile;
--- frontend/pages/profile/index.tsx
+++ frontend/pages/profile/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../../components/bar/Navbar";
+import Profile from "../../components/profile/Profile";
 
 const Index = () => {
   return (
     <div className="flex flex-row" style={{ width: "100vw", height: "100vh" }}>
       <Navbar />
+      <Profile />
     </div>
   );
 };
