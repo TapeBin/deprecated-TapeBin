9d19468fad6bda11eb489bccda4a7f7e222f577d
B. Düdük <bunyamin.duduk@hva.nl>
2021-10-30 16:29:05 +0200

    Added profile page and component

---
+++ frontend/pages/profile/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/bar/Navbar";
+
+const Index = () => {
+  return (
+    <div className="flex flex-row" style={{ width: "100vw", height: "100vh" }}>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Index;
