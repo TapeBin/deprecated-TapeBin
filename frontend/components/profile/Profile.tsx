b9e76a176fff300480b62fadee623fdeeb3e91b8
B. Düdük <bunyamin.duduk@hva.nl>
2021-10-30 16:49:47 +0200

    Implemented profile further

--- frontend/components/profile/Profile.tsx
+++ frontend/components/profile/Profile.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TopProfile from "./TopProfile";
 
 const Profile = () => {
-  return <div className="flex flex-col"></div>;
+  return (
+    <div className="flex flex-col px-[25px] py-[35px] bg-gray-800 w-full h-full ">
+      <TopProfile />
+    </div>
+  );
 };
 
 export default Profile;
--- frontend/components/profile/TopProfile.tsx
+++ frontend/components/profile/TopProfile.tsx
@@ -1,7 +1,21 @@
+import { useAtom } from "jotai";
 import React from "react";
+import { userAtom } from "../../states/user";
 
 const TopProfile = () => {
-  return <div></div>;
+  const [user] = useAtom(userAtom);
+  return (
+    <div className="flex flex-row items-center space-x-8 px-[40px] py-[20px] rounded-md bg-gray-500 text-gray-100">
+      <img
+        src={user.profileImage}
+        alt="Profile Image"
+        className="h-[90px] rounded-full"
+      />
+      <div className="flex flex-col text-3xl">
+        <div>{user.username}</div>
+      </div>
+    </div>
+  );
 };
 
 export default TopProfile;
