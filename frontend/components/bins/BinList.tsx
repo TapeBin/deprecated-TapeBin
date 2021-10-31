b5be4f1e04805975ac58f223cc1aac91cd31ad7e
B. Düdük <bunyamin.duduk@hva.nl>
2021-10-31 15:11:19 +0100

    Adding a bin now works

--- frontend/components/bins/BinList.tsx
+++ frontend/components/bins/BinList.tsx
@@ -7,16 +7,30 @@ import BinItem from "../form/binlist/BinItem";
 
 const BinList = () => {
   const [loaded, setLoaded] = useState(false);
-  const [bins] = useAtom(binsAtom);
+  const [bins, setBins] = useAtom(binsAtom);
   const [editor] = useAtom(editorAtom);
 
   useEffect(() => {
     if (bins.bins.length === 0) {
       bins.bins.push(new Bin(0, "New File", 0, editor.mode));
     }
-
     setLoaded(true);
   }, []);
+
+  const addBin = () => {
+    const newArray: Bin[] = bins.bins;
+    let max = 0;
+    if (newArray.length > 0) {
+      newArray.forEach((bin) => {
+        if (bin.id > max) max = bin.id;
+      });
+    }
+
+    const newBin = new Bin(max, "New File", 0, editor.mode);
+    newArray.push(newBin);
+
+    setBins((prevState) => ({ ...prevState, bins: newArray }));
+  };
 
   return (
     <>
@@ -24,7 +38,10 @@ const BinList = () => {
         bins.bins.map((bin: Bin) => (
           <BinItem fileName={bin.fileName} id={bin.id} key={bin.id} />
         ))}
-      <button className="rounded-md py-2 px-4 border-2 border-transparent hover:border-2 hover:border-proColor">
+      <button
+        className="rounded-md py-2 px-4 border-2 border-transparent hover:border-2 hover:border-proColor"
+        onClick={addBin}
+      >
         +
       </button>
     </>
