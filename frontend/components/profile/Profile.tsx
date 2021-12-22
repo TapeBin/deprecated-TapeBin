3e48d89713c1989df287f67c85de2b623ff75444
Bunyamin Duduk <bunyamin.duduk@hva.nl>
2021-12-22 21:56:14 +0100

    Fixed profile binlist overflow

--- frontend/components/profile/Profile.tsx
+++ frontend/components/profile/Profile.tsx
@@ -1,18 +1,22 @@
 import React, { FunctionComponent } from "react";
 import TopProfile from "./TopProfile";
 import ProfileBinList from "./ProfileBinList";
+import Footer from "../footer/Footer";
 
 type ProfileProps = {
-  bins: any[];
+    bins: any[];
 };
 
 const Profile: FunctionComponent<ProfileProps> = (props: ProfileProps) => {
-  return (
-    <div className="flex flex-col px-[25px] py-[35px] bg-gray-800 w-full h-full space-y-5">
-      <TopProfile amountOfBins={props.bins.length}/>
-      <ProfileBinList bins={props.bins}/>
-    </div>
-  );
+    return (
+        <div className="flex flex-col overflow-y-auto px-[25px] pt-[35px] pb-4 bg-gray-800 w-full h-full space-y-5">
+            <TopProfile amountOfBins={props.bins.length}/>
+            <ProfileBinList bins={props.bins}/>
+            <div className="mt-16">
+                <Footer/>
+            </div>
+        </div>
+    );
 };
 
 export default Profile;
