21141d1f3f2d9dcb46b413233b76a4bb1006cd31
PhelixTaken <thegggameyt@gmail.com>
2022-01-24 20:16:13 +0100

    Added copying current bin text

--- frontend/components/bar/Navbar.tsx
+++ frontend/components/bar/Navbar.tsx
@@ -6,11 +6,15 @@ import Icon from "./Icon";
 import Logo from "./Logo";
 import ProfileIcon from "./ProfileIcon";
 import { pageAtom } from "../../pages/_app";
+import {binFormAtom} from "../../states/binForm";
+import {binsAtom} from "../../states/bins";
 
 const Navbar = () => {
   const router = useRouter();
   const [user] = useAtom(userAtom);
   const [page, setPage] = useAtom(pageAtom);
+  const [binForm] = useAtom(binFormAtom);
+  const [bins] = useAtom(binsAtom);
 
   const logIn = () => {
     if (user.isLoggedIn) {
@@ -30,12 +34,18 @@ const Navbar = () => {
     localStorage.setItem("isBarHidden", `${toggle}`);
   };
 
+  const copyCurrentBin = () => {
+    const bin = bins.bins.filter(foundBin => foundBin.id === binForm.currentBinId)[0];
+    navigator.clipboard.writeText(bin.text);
+  }
+
   return (
     <nav className="w-[70px] sticky top-0 flex-none block overflow-y-auto  h-full bg-background flex flex-col space-y-12 justify-start items-center border-r-2 border-gray-700">
       <Logo />
       <div className="flex flex-col space-y-5 items-center">
         <ProfileIcon src={user.profileImage} alt="Profile" onClick={logIn} />
         <Icon src="/images/settings.svg" alt="Settings" onClick={redirectToSettings}/>
+        <Icon src="/images/clipboard-file-outline.svg" alt="Copy" onClick={copyCurrentBin}/>
         <Icon src={page.isBarHidden ? "/images/arrow-collapse-right.svg" : "/images/arrow-collapse-left.svg"} alt={"Show/Hide"} onClick={toggleNavbar}/>
       </div>
     </nav>
